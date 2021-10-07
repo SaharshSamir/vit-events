c5d90d97fc8314926609accca43948b301e42670
SaharshSamir <saharshsamir10@gmail.com>
2021-10-07 23:18:15 +0530

    misc

--- dist/js/config/keys.js
+++ dist/js/config/keys.js
@@ -1,13 +1,9 @@
 "use strict";
-<<<<<<< HEAD
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production")
+{
     module.exports = require("./prod");
 }
-else {
+else
+{
     module.exports = require("./dev");
 }
-=======
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MONGOOSE_URI = void 0;
-exports.MONGOOSE_URI = "";
->>>>>>> 04adf7f5fd6365694b5bff42a1f8c3989378d63a
