753615fea870d0514b15d34e87d16d75a9b0db78
SaharshSamir <saharshsamir10@gmail.com>
2021-11-15 04:21:52 +0530

    Merge branch 'master' of https://github.com/SaharshSamir/vit-events

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Test = () => {
+const Test: React.FC = () => {
 	return (
 		<>
 			<Container></Container>
@@ -10,5 +10,7 @@ const Test = () => {
 };
 
 const Container = styled.div``;
+const FormContainer = styled.form``;
+const Label = styled.p``;
 
 export default Test;
